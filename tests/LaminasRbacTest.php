a0a30117dcc612a321406e265d81517621f10eb3
Thomas Müller <thomas.mueller@geld.de>
2020-11-02 09:41:37 +0100

    add new tests

--- tests/LaminasRbacTest.php
+++ tests/LaminasRbacTest.php
@@ -11,10 +11,13 @@
 declare(strict_types = 1);
 namespace MezzioTest\GenericAuthorization\Rbac;
 
+use Laminas\Permissions\Rbac\Exception\InvalidArgumentException;
 use Laminas\Permissions\Rbac\Rbac;
+use Mezzio\GenericAuthorization\Exception\RuntimeException;
 use Mezzio\GenericAuthorization\Rbac\LaminasRbac;
 use Mezzio\GenericAuthorization\Rbac\LaminasRbacAssertionInterface;
 use PHPUnit\Framework\TestCase;
+use Psr\Http\Message\ServerRequestInterface;
 
 final class LaminasRbacTest extends TestCase
 {
@@ -106,5 +109,76 @@ final class LaminasRbacTest extends TestCase
         self::assertInstanceOf(LaminasRbac::class, $laminasRbac);
 
         self::assertTrue($laminasRbac->isGranted($role, $resource));
+    }
+
+    /**
+     * @throws \Mezzio\GenericAuthorization\Exception\RuntimeException
+     * @throws \PHPUnit\Framework\ExpectationFailedException
+     * @throws \PHPUnit\Framework\MockObject\RuntimeException
+     * @throws \SebastianBergmann\RecursionContext\InvalidArgumentException
+     *
+     * @return void
+     */
+    public function testIsGrantedWitAssertionException(): void
+    {
+        $role      = 'foo';
+        $resource  = 'bar';
+        $assertion = $this->createMock(LaminasRbacAssertionInterface::class);
+
+        $rbac = $this->getMockBuilder(Rbac::class)
+            ->disableOriginalConstructor()
+            ->getMock();
+        $rbac->expects(self::once())
+            ->method('isGranted')
+            ->with($role, $resource, $assertion)
+            ->willThrowException(new InvalidArgumentException('test'));
+
+        /** @var Rbac $rbac */
+        /** @var LaminasRbacAssertionInterface $assertion */
+        $laminasRbac = new LaminasRbac($rbac, $assertion);
+        self::assertInstanceOf(LaminasRbac::class, $laminasRbac);
+
+        $this->expectException(RuntimeException::class);
+        $this->expectExceptionMessage('Could not check Authorization');
+
+        $laminasRbac->isGranted($role, $resource);
+    }
+
+    /**
+     * @throws \Mezzio\GenericAuthorization\Exception\RuntimeException
+     * @throws \PHPUnit\Framework\ExpectationFailedException
+     * @throws \PHPUnit\Framework\MockObject\RuntimeException
+     * @throws \SebastianBergmann\RecursionContext\InvalidArgumentException
+     *
+     * @return void
+     */
+    public function testIsGrantedWitAssertionAndRequest(): void
+    {
+        $role     = 'foo';
+        $resource = 'bar';
+
+        $request = $this->createMock(ServerRequestInterface::class);
+
+        $assertion = $this->getMockBuilder(LaminasRbacAssertionInterface::class)
+            ->disableOriginalConstructor()
+            ->getMock();
+        $assertion->expects(self::once())
+            ->method('setRequest')
+            ->with($request);
+
+        $rbac = $this->getMockBuilder(Rbac::class)
+            ->disableOriginalConstructor()
+            ->getMock();
+        $rbac->expects(self::once())
+            ->method('isGranted')
+            ->with($role, $resource, $assertion)
+            ->willReturn(true);
+
+        /** @var Rbac $rbac */
+        /** @var LaminasRbacAssertionInterface $assertion */
+        $laminasRbac = new LaminasRbac($rbac, $assertion);
+        self::assertInstanceOf(LaminasRbac::class, $laminasRbac);
+
+        self::assertTrue($laminasRbac->isGranted($role, $resource, null, $request));
     }
 }
